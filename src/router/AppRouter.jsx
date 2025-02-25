cc0c0cc5c1f5013ff5b98c2f20bc364d71df2c4a
Cristopher <cris220802@hotmail.com>
2025-02-25 13:07:52 -0600

    cris 25.02.25

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Route, Routes } from 'react-router'
+import { Login } from '../pages/Login'
+
+export const AppRouter = () => {
+  return (
+    <>
+        <Routes>
+            <Route path="/" element={<Login />}/>
+            {/* <Route path="/dashboard" element={<Login />}/> */}
+            {/* <Route path="/dashboard/ensayista" element={<Login />}/> */}
+        </Routes>
+    </>
+  )
+}
